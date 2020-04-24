182505f717573bc21d7ddee6506e2de24e439fa3
Marius Zerni <marius_zerni@yahoo.com>
2020-04-24 10:32:12 +0100

    getting deployed

--- project/settings.py
+++ project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -145,3 +145,5 @@ REST_FRAMEWORK = {
 MEDIA_URL =  '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 print(MEDIA_ROOT)
+
+django_heroku.settings(locals())
